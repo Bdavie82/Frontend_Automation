374569c06f800859101c9502458ae2e8abc5fd26
ryell22 <DR@GONb@llz22>
2018-04-10 17:38:04 -0600

    Initial commit

---
+++ src/WebsiteTesting/Java/LogicObjects/SeleniumTests.java
@@ -0,0 +1,14 @@
+package WebsiteTesting.Java.LogicObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.*;
+
+
+public class SeleniumTests {
+
+    public WebDriver get_webdriver() {
+        return _webdriver;
+    }
+}
